f9c8c35d0e54921aab7de6c5d69123fb04a849c6
Jan Jaap Braam <jjbraam@gmail.com>
2020-01-01 16:19:13 +0100

    fix regex and refac

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-let lastContainer = null
+let lastContainer = null;
+const bracketRegEx = /-?\[[^\]]*|[^[]*\]/g;
 
 function removeClosedCaptions() {
-  const els = document.getElementsByClassName('player-timedtext-text-container')
+    const els = document.getElementsByClassName('player-timedtext-text-container');
 
-  if (els.length === 1) {
-    const el = els[0]
+    if (els.length === 1) {
+        const el = els[0];
 
-    if (el != lastContainer && el.children != null) {
-      lastContainer = el
+        if (el != lastContainer && el.children != null) {
+            lastContainer = el;
 
-      for (const e of el.children) {
-        for (const n of e.childNodes) {
-          if (n.nodeValue)
-            n.nodeValue = n.nodeValue.replace(/ *-? *\[.+?\] */g, '')
+            for (const e of el.children) {
+                if (e.textContent) {
+                    
+                    if(bracketRegEx.test(e.textContent)) {
+                        console.log(e.textContent);
+                        e.textContent = e.textContent.replace(bracketRegEx, '');
+                    }
+                    
+                }
+            }
         }
-      }
     }
-  }
 
-  requestAnimationFrame(removeClosedCaptions)
+    requestAnimationFrame(removeClosedCaptions);
 }
 
-removeClosedCaptions()
+removeClosedCaptions();
